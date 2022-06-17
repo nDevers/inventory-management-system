28aa9c94acbd35cbd5d24504a4326ee1456207ac
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-06-17 11:31:08 +0600

    footer created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   );
 }
