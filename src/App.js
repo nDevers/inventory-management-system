935eafa9efa65f7d921c02fb6e96df5320d3053a
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-06-17 11:18:57 +0600

    installed daisy ui and created navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Navbar />
     </div>
   );
 }
