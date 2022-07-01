4be457aff1661842dab253b3f4157d61983059cd
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-01 11:12:34 +0600

    website theme changed to white

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import { Routes, Route } from "react-router-dom";
-import Footer from "./components/Footer";
-import DefaultNavbar from "./components/DefaultNavbar";
-import Login from "./pages/Authentication/Login";
-import Register from "./pages/Authentication/Register";
+import './App.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Home from "./pages/Home/Home";
-import Dashboard from "./pages/Dashboard/Dashboard";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import Features from './Pages/Features/Features';
+import CustomerStories from './Pages/CustomerStories/CustomerStories';
+import FindExperts from './Pages/FindExperts/FindExperts';
+import ForgotPassword from './Pages/ForgotPassword/ForgotPassword';
+import Integrations from './Pages/Integrations/Integrations';
+import Login from './Pages/Login/Login';
+import Pricing from './Pages/Pricing/Pricing';
+import Register from './Pages/Register/Register';
+import Resources from './Pages/Resources/Resources';
+import NotFound from './Pages/NotFound/NotFound';
+import About from './Pages/About/About';
+import Contact from './Pages/Contact/Contact';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import DashboardSummary from './Pages/Dashboard/DashboardSummary';
+import Products from './Pages/Dashboard/Products/Products';
+import RequiredItems from './Pages/Dashboard/RequiredItems';
+import Orders from './Pages/Dashboard/Orders';
+import Purchases from './Pages/Dashboard/Purchases';
+import Returns from './Pages/Dashboard/Returns';
+import Employees from './Pages/Dashboard/Employees';
+import Customers from './Pages/Dashboard/Customers';
+import Suppliers from './Pages/Dashboard/Suppliers';
+import Setup from './Pages/Dashboard/Setup';
+import CreateProduct from './Pages/Dashboard/Products/CreateProduct';
 
 function App() {
   return (
-    <div>
-
+    <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="home" element={<Home />} />
+        <Route path="features" element={<Features />} />
+        <Route path="customer-stories" element={<CustomerStories />} />
+        <Route path="find-experts" element={<FindExperts />} />
+        <Route path="forgot-password" element={<ForgotPassword />} />
+        <Route path="integration" element={<Integrations />} />
         <Route path="login" element={<Login />} />
+        <Route path="pricing" element={<Pricing />} />
         <Route path="register" element={<Register />} />
+        <Route path="resources" element={<Resources />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="dashboard" element={<Dashboard />}>
+          <Route index element={<DashboardSummary />} />
+          <Route path='products' element={<Products />} />
+          <Route path='create-product' element={<CreateProduct />} />
+          <Route path='required-items' element={<RequiredItems />} />
+          <Route path='orders' element={<Orders />} />
+          <Route path='purchases' element={<Purchases />} />
+          <Route path='setup' element={<Setup />} />
+          <Route path='returns' element={<Returns />} />
+          <Route path='employees' element={<Employees />} />
+          <Route path='customers' element={<Customers />} />
+          <Route path='suppliers' element={<Suppliers />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
-
-      <Footer />
       <ToastContainer />
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,19 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        simpleInventory: {
+          primary: "#a991f7",
+          secondary: "#f6d860",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "light",
+    ],
+  },
   plugins: [require("daisyui", '@tailwindcss/forms')],
 }
