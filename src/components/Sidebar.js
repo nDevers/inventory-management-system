54adcab4e3c474b020f062efa6a57ef7e79f2fa6
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-01 17:09:46 +0600

    sidebar icons added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { FaProductHunt } from 'react-icons/fa';
+import { BiPurchaseTagAlt } from 'react-icons/bi';
+import { FaUsers } from 'react-icons/fa';
+import { FiSettings } from 'react-icons/fi';
+import { HiTemplate } from 'react-icons/hi';
+import { MdOutlineSummarize, MdOutlineAddBox } from 'react-icons/md';
+import { TbTruckReturn, TbTruckDelivery } from 'react-icons/tb';
+import { RiProductHuntFill, RiAdminFill, RiShoppingCartFill } from 'react-icons/ri';
 import { Link, Outlet } from 'react-router-dom';
 
 const Sidebar = () => {
@@ -13,18 +19,73 @@ const Sidebar = () => {
             </div>
             <div class="drawer-side">
                 <label for="my-drawer-2" class="drawer-overlay"></label>
-                <ul class="menu p-4 overflow-y-auto w-52 bg-base-100 text-base-content uppercase">
-                    <li className='flex'><Link to=''>Summary</Link></li>
-                    <li className='flex'><Link to='products'>Products</Link></li>
-                    <li className='flex'><Link to='create-product'>Create Product</Link></li>
-                    <li><Link to='required-items'>Required Items</Link></li>
-                    <li><Link to='orders'>Orders</Link></li>
-                    <li><Link to='purchases'>Purchases</Link></li>
-                    <li><Link to='setup'>Setup</Link></li>
-                    <li><Link to='returns'>Returns</Link></li>
-                    <li><Link to='employees'>Employees</Link></li>
-                    <li><Link to='customers'>Customers</Link></li>
-                    <li><Link to='suppliers'>Suppliers</Link></li>
+                <ul class="menu p-4 overflow-y-auto w-52 text-[13px] font-semibold uppercase">
+                    <li className='flex'>
+                        <Link to=''>
+                            <MdOutlineSummarize className='text-lg' />
+                            Summary
+                        </Link>
+                    </li>
+                    <li className='flex'>
+                        <Link to='products'>
+                            <RiProductHuntFill className='text-lg' />
+                            Products
+                        </Link>
+                    </li>
+                    <li className='flex'>
+                        <Link to='create-product'>
+                            <MdOutlineAddBox className='text-lg' />
+                            Create Product
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='required-items'>
+                            <HiTemplate className='text-lg' />
+                            Required Items
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='orders'>
+                            <RiShoppingCartFill className='text-lg' />
+                            Orders
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='purchases'>
+                            <BiPurchaseTagAlt className='text-lg' />
+                            Purchases
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='setup'>
+                            <FiSettings className='text-lg' />
+                            Setup
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='returns'>
+                            <TbTruckReturn className='text-lg' />
+                            Returns
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='employees'>
+                            <RiAdminFill className='text-lg' />
+                            Employees
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='customers'>
+                            <FaUsers className='text-lg' />
+                            Customers
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to='suppliers'>
+                            <TbTruckDelivery className='text-lg' />
+                            Suppliers
+                        </Link>
+                    </li>
                 </ul>
             </div>
         </div>
