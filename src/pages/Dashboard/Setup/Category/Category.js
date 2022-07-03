ca3eaa2fd08bb0a5f210c46587fccd7e98979e56
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-03 23:05:03 +0600

    create a category

--- src/pages/Dashboard/Setup/Category/Category.js
+++ src/pages/Dashboard/Setup/Category/Category.js
@@ -18,8 +18,38 @@ const Category = () => {
 
     return (
         <section>
+            <input type="checkbox" id="create-new-category" class="modal-toggle" />
+            <label for="create-new-category" class="modal cursor-pointer">
+                <label class="modal-box w-2/5 h-2/5 max-w-4xl relative p-4" for="">
+                    <label for="create-new-category" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+
+                    <h3 class="font-bold text-lg text-center uppercase mb-2">Create a Category</h3>
+
+                    <div className='grid grid-cols-2 gap-x-4 place-items-center'>
+                        <div>
+                            <label class="label">
+                                <span class="label-text">Name</span>
+                            </label>
+                            <input type="text" placeholder="Type here" class="input input-xs input-bordered" />
+                        </div>
+
+                        <div className="div">
+                            <label class="label">
+                                <span class="label-text">Type</span>
+                            </label>
+                            <select class="select select-bordered select-xs">
+                                <option disabled selected>Small</option>
+                                <option>Small Apple</option>
+                                <option>Small Orange</option>
+                                <option>Small Tomato</option>
+                            </select>
+                        </div>
+                    </div>
+                </label>
+            </label>
+
             <div className="flex justify-between mb-6">
-                <label for="create-new-product" class="btn btn-sm gap-2 modal-button">
+                <label for="create-new-category" class="btn btn-sm gap-2 modal-button">
                     <MdOutlineAddBox className='text-lg' />
                     New
                 </label>
