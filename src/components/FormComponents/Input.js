d0c5672e9e6de07bb4fb96a77a167ffe6ad7b49a
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-20 22:28:22 +0600

    input name field

--- src/components/FormComponents/Input.js
+++ src/components/FormComponents/Input.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Input = ({ title = 'Input Field' }) => {
+const Input = ({ title = 'Input Field', name }) => {
     return (
         <div>
             <label class="label">
                 <span class="label-text">{title}</span>
             </label>
-            <input type="text" placeholder="Type here" class="input input-xs input-bordered w-full max-w-xs" />
+            <input type="text" placeholder="Type here" class="input input-xs input-bordered w-full max-w-xs" name={name} />
         </div>
     );
 };
