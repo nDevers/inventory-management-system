69288f189fef86b0d472ddcc31a6992d39fa9333
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-21 22:20:28 +0600

    requested items routes margin

--- src/pages/Dashboard/RequestedItems/NonPharmacy/NonPharmacyItems.js
+++ src/pages/Dashboard/RequestedItems/NonPharmacy/NonPharmacyItems.js
@@ -59,7 +59,7 @@ const NonPharmacyItems = () => {
 
     return (
         <section className='p-4'>
-            <div className="flex justify-between items-center mb-8">
+            <div className="flex justify-between items-center mb-4">
                 <h2 className='text-2xl text-center font-bold'>Requested Non Pharmacy Items</h2>
 
                 <div className='flex items-center gap-x-4'>
