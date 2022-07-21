090a6f1526b7f190b510bcf8affd8a8c31edb51b
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-21 11:51:30 +0600

    dashboard margin

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -3,7 +3,7 @@ import Sidebar from '../../components/Sidebar';
 
 const Dashboard = () => {
     return (
-        <div className='mx-1 md:mx-3'>
+        <div>
             <Sidebar />
         </div>
     );
