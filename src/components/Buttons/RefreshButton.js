560819569160a39b6d81d52c461777c28a956f76
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-22 01:08:12 +0600

    onclick refresh non pharmacy page

--- src/components/Buttons/RefreshButton.js
+++ src/components/Buttons/RefreshButton.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { FiRefreshCcw } from 'react-icons/fi';
 
 const RefreshButton = ({ btnSize }) => {
+    console.log('refresh')
+
     return (
-        <button class={`btn ${btnSize} flex gap-x-2`} >
+        <button class={`btn ${btnSize} flex gap-x-2`} onClick={() => window.location.reload(true)} >
             <FiRefreshCcw className='text-md' />
             Refresh
         </button >
