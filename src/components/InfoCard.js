608ec1a742b05655ff6d890423c7c82352e005f9
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-22 13:35:42 +0600

    summary card color

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const InfoCard = ({ name, status }) => {
     return (
-        <div className="flex flex-col items-center justify-center gap-2 bg-teal-500 text-white rounded-md p-2">
-            <p className='font-semibold'>{name}</p>
+        <div className="flex flex-col items-center justify-center gap-2 bg-teal-400 text-white rounded-md p-2">
+            <p className='font-semibold text-center'>{name}</p>
             <h3 className="text-xl font-bold badge badge-lg badge-secondary">+{status}</h3>
         </div>
     );
