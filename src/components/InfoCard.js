a67808b17bd01436e55602ec0e8f93f7622edf88
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-22 13:36:56 +0600

    + sign removed from card

--- src/components/InfoCard.js
+++ src/components/InfoCard.js
@@ -4,7 +4,7 @@ const InfoCard = ({ name, status }) => {
     return (
         <div className="flex flex-col items-center justify-center gap-2 bg-teal-400 text-white rounded-md p-2">
             <p className='font-semibold text-center'>{name}</p>
-            <h3 className="text-xl font-bold badge badge-lg badge-secondary">+{status}</h3>
+            <h3 className="text-xl font-bold badge badge-lg badge-secondary">{status}</h3>
         </div>
     );
 };
