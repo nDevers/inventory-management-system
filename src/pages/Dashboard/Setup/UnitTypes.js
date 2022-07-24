bb86e319c2a214e9b6cbbacbcdb474c94b9deb14
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-24 23:15:27 +0600

    unit type removed & refresh btn addded

--- src/pages/Dashboard/Setup/UnitTypes.js
+++ src/pages/Dashboard/Setup/UnitTypes.js
@@ -3,12 +3,12 @@ import PrintButton from '../../../components/Buttons/PrintButton';
 import Input from '../../../components/FormComponents/Input';
 import TableRow from '../../../components/TableRow';
 import SaveButton from '../../../components/Buttons/SaveButton';
-import Select from '../../../components/FormComponents/Select';
 import EditButton from '../../../components/Buttons/EditButton';
 import DeleteButton from '../../../components/Buttons/DeleteButton';
+import RefreshButton from '../../../components/Buttons/RefreshButton';
 
 const UnitTypes = () => {
-    const tableHeadItems = ['SN', 'Name', 'Description', 'Type', 'Creator', 'Created At', 'Updated By', 'Updated At', 'Actions'];
+    const tableHeadItems = ['SN', 'Name', 'Description', 'Creator', 'Created At', 'Updated By', 'Updated At', 'Actions'];
 
     const tableHead = <tr>
         {
@@ -21,13 +21,12 @@ const UnitTypes = () => {
 
         const name = event?.target?.unitName?.value;
         const description = event?.target?.unitDescription?.value;
-        const type = event?.target?.unitUnitType?.value;
         const addedBy = 'admin';
         const addedTime = new Date();
         const updatedBy = 'admin';
         const updatedTime = new Date();
 
-        const unitTypeDetails = { name, description, type, addedBy, addedTime, updatedBy, updatedTime };
+        const unitTypeDetails = { name, description, addedBy, addedTime, updatedBy, updatedTime };
 
         // send data to server
         fetch('https://stringlab-ims-server.herokuapp.com/api/setup/unitTypes', {
@@ -61,6 +60,7 @@ const UnitTypes = () => {
 
                     <div className='flex items-center gap-x-4'>
                         <SaveButton btnSize='btn-xs' />
+                        <RefreshButton btnSize='btn-xs' />
                         <PrintButton btnSize='btn-xs' />
                     </div>
                 </div>
@@ -69,12 +69,6 @@ const UnitTypes = () => {
                     <div className='flex place-items-center gap-4 mt-4 mb-8'>
                         <Input title={'Unit Name'} name='unitName' isRequired='required' />
                         <Input title={'Description'} name='unitDescription' isRequired='required' />
-                        <Select
-                            title={'Unit Type'}
-                            name='unitUnitType'
-                            isRequired='required'
-                            options={['Pharmacy', 'Non Pharmacy']}
-                        />
                     </div>
                 </div>
             </form>
@@ -95,11 +89,10 @@ const UnitTypes = () => {
                                         index + 1,
                                         category.name,
                                         category.description,
-                                        category.type,
                                         category.addedBy,
-                                        category.addedTime,
+                                        category?.addedTime?.slice(0, 10),
                                         category.updatedBy,
-                                        category.updatedTime,
+                                        category?.updatedTime?.slice(0, 10),
                                         <span className='flex items-center gap-x-1'>
                                             <EditButton />
                                             <DeleteButton
