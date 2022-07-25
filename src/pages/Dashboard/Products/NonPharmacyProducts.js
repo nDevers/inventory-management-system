e2ffa27cf60ab6a7ee952ad5a4393fe0d77242c0
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-25 09:49:28 +0600

    total non pharmacy products

--- src/pages/Dashboard/Products/NonPharmacyProducts.js
+++ src/pages/Dashboard/Products/NonPharmacyProducts.js
@@ -90,7 +90,7 @@ const NonPharmacyProducts = () => {
     return (
         <section className='lg:p-4 md:p-2 p-1'>
             <div className="flex flex-col md:flex-row lg:flex-row justify-between items-center gap-y-2 mb-6">
-                <h2 className='lg:text-2xl md:text-xl text-lg text-center font-bold'>Non Pharmacy Products</h2>
+                <h2 className='lg:text-2xl md:text-xl text-lg text-center font-bold'>Non Pharmacy Products: <span className='badge badge-lg badge-secondary'>{nonPharmacyProducts.length}</span></h2>
 
                 <div className='flex items-center gap-x-4'>
                     <NewButton modalId='create-new-product' btnSize='btn-xs' />
