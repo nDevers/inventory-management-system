464c7f8249d5aad6eabe8d0464d7fc2813242d50
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-07-26 07:28:31 +0600

    settings page

--- src/pages/User/Settings.js
+++ src/pages/User/Settings.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Footer from '../../components/Footer';
+import UserNavbar from '../../components/UserNavbar';
 
 const Settings = () => {
     return (
         <div>
-            <h2>Settings</h2>
+            <UserNavbar />
+            <div className="container my-24 px-6 mx-auto">
+                <section className="mb-32 text-gray-800">
+                    <h2>Settings</h2>
+                </section>
+            </div>
+            <Footer />
         </div>
     );
 };
